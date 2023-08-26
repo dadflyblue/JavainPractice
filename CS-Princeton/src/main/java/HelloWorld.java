099df11ff92e10b98e27980e9f18374bc37ffff5
Haibo Luo <haibo_luo@icloud.com>
2023-08-26 19:11:31 -0400

    remove courses from practive repo

--- CS-Princeton/src/main/java/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld {
-
-  // specification: https://coursera.cs.princeton.edu/introcs/assignments/hello/specification.php
-  // faq: https://coursera.cs.princeton.edu/introcs/assignments/hello/faq.php
-
-  public static void main(String[] args) {
-    System.out.println("Hello, World");
-  }
-}
--- CS-Princeton/src/main/java/Sqrt.java
+++
@@ -1,7 +0,0 @@
-public class Sqrt {
-  public static void main(String[] args) {
-    double x = Double.parseDouble(args[0]);
-
-    System.out.println(String.format("%.2f", Newton.sqrt(x, 1e-2)));
-  }
-}
